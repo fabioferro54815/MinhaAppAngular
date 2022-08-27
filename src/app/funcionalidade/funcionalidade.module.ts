2b0c8c2461f2cfc1edcfbafe3ce6df9f43160700
Fabio Ferro Junior <fabioferrojunior2@gmail.com>
2022-08-27 13:42:31 -0300

    Comeco do projeto

---
+++ src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    //Seus componentes
+  ],
+  imports: [
+    CommonModule
+    //Seus Modulos
+  ],
+  providers: [
+    //Seus Serviços
+  ]
+})
+export class FuncionalidadeModule { }
